c1626aa247f7a8a91cf6845a07b2733cb170873f
amir-memarian <memarian.email@gmail.com>
2023-05-08 20:32:26 +0330

    Interface

--- AdvanceJavaProject/src/main/java/khodro/Mashin.java
+++ AdvanceJavaProject/src/main/java/khodro/Mashin.java
@@ -1,6 +1,6 @@
 package khodro;
 
-public class Mashin extends Khodro {
+public class Mashin extends Khodro implements SandoghDar, GearBox {
     boolean ayaDarBazAst;
     String mark;
     Ranande ranande;
@@ -21,5 +21,15 @@ public class Mashin extends Khodro {
         }else{
             return false;
         }
+    }
+
+    @Override
+    public String typeDande() {
+        return "Automatic";
+    }
+
+    @Override
+    public int gonjayeshSandogh() {
+        return 100;
     }
 }
