c5bbeb730648057b8decd21cc5caa51341f5d3a7
amir-memarian <memarian.email@gmail.com>
2023-05-12 10:25:00 +0330

    Calculator Change code

--- AdvanceJavaProject/src/main/java/calculator/CalculatorUI.java
+++ AdvanceJavaProject/src/main/java/calculator/CalculatorUI.java
@@ -3,6 +3,7 @@ package calculator;
 import sun.text.normalizer.UCharacter;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CalculatorUI {
 
@@ -27,37 +28,46 @@ public class CalculatorUI {
         String[][] label = {{"1","2","3","+"},{"4","5","6","-"},{"7","8","9","x"},{".","0","=","/"}};
         for(int rowIndex=0;rowIndex < 4;rowIndex++){
             for(int colIndex=0;colIndex < 4;colIndex++){
-                if (colIndex<3) {
-                    jButton[rowIndex][colIndex] = getButton(label[rowIndex][colIndex], colIndex * 50, (rowIndex + 1) * 50, false);
-                }else{
-                    jButton[rowIndex][colIndex] = getButton(label[rowIndex][colIndex], colIndex * 50, (rowIndex + 1) * 50,  true);
+                if ((rowIndex == 3) && (colIndex == 2)){
+                    jButton[rowIndex][colIndex] = getButton(label[rowIndex][colIndex], colIndex * 50, (rowIndex + 1) * 50, "Equal");
+                }else {
+                    if (colIndex < 3) {
+                        jButton[rowIndex][colIndex] = getButton(label[rowIndex][colIndex], colIndex * 50, (rowIndex + 1) * 50, "Number");
+                    } else {
+                        jButton[rowIndex][colIndex] = getButton(label[rowIndex][colIndex], colIndex * 50, (rowIndex + 1) * 50, "Operation");
+                    }
                 }
                 mashinHesab.add(jButton[rowIndex][colIndex]);
             }
         }
 
-        jButton[3][2].addActionListener(e -> {
-            Calculator calculator = new Calculator();
-            String addResult = calculator.calcute(adadAval, jTextField.getText(), oper);
-            jTextField.setText(addResult);
-        });
-
-        mashinHesab.setSize(600,600);
+        mashinHesab.setSize(220,300);
         mashinHesab.setLayout(null);
         mashinHesab.setVisible(true);
     }
 
-    private static JButton getButton(String lable, int xPosition, int yPosition, boolean operation) {
+    private static JButton getButton(String lable, int xPosition, int yPosition, String type) {
         JButton point = new JButton(lable);
         point.setBounds(xPosition, yPosition, 50, 50);
-        if (operation == true){
+        if (type.equals("Operation")){
             point.addActionListener(event -> {
-                adadAval = jTextField.getText();
-                oper = point.getText();
-                jTextField.setText("");
+                    adadAval = jTextField.getText();
+                    oper = point.getText();
+                    jTextField.setText("");
+            });
+        } else if (type.equals("Number")){
+            point.addActionListener(event -> {
+                if (jTextField.getText().indexOf(".") == -1) {
+                    jTextField.setText(jTextField.getText() + point.getText());
+                }
             });
         } else {
-            point.addActionListener(event -> jTextField.setText(jTextField.getText() + point.getText()));
+            point.addActionListener(event -> {
+                Calculator calculator = new Calculator();
+                String addResult = calculator.calcute(adadAval, jTextField.getText(), oper);
+                adadAval = jTextField.getText();
+                jTextField.setText(addResult);
+            });
         }
         return point;
     }
